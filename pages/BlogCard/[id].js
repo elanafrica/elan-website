101cd3d44e7a44e25ad7b55f61f8038a9b4ce1b7
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-05-16 08:49:06 +0100

    update

--- pages/BlogCard/[id].js
+++ pages/BlogCard/[id].js
@@ -9,18 +9,21 @@ function BlogPost() {
   const posts = [
     {
       id: 1,
+      image: "",
       title: "How to maximize your marketing budget",
       content:
         "Marketing plays a crucial role in the growth and success of any business. However, with limited..",
     },
     {
       id: 2,
+      image: "",
       title: "11 marketing strategies to attract and retain",
       content:
         "Increasing revenue is principal to the growth of any business. It's not enough to have a viable product, build an excellent team or even receive the right investment for your business–marketing allows",
     },
     {
       id: 3,
+      image: "",
       title: "Is Email marketing Dead?",
       content:
         "To build a successful business in Africa, here are 5 free courses",
