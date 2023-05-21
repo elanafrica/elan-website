302464d0c9fb9a186deb5a0c9a95bb2e63475fb0
elanafrica <team@elanafrica.com>
2023-05-21 15:27:25 +0100

    latest contact

---
+++ pages/api/contact.js
@@ -0,0 +1,13 @@
+export default function (req, res) {
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: "demo@demo.gmail",
+            pass: "password",
+        },
+        secure: true,
+    });
+    console.log(req.body)
+}
