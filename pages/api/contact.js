b5590ea6cbb7d45223261c7a534c4df557012862
elanafrica <team@elanafrica.com>
2023-05-22 14:23:11 +0100

    sendmail

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,13 +1,33 @@
+require('dotenv').config()
+// const PASSWORD = process.env.password;
+
 export default function (req, res) {
+    require('dotenv').config()
+
     let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
         port: 465,
         host: "smtp.gmail.com",
         auth: {
-            user: "demo@demo.gmail",
-            pass: "password",
+            user: "elanafrica753@gmail.com",
+            pass: "process.env.password",
         },
         secure: true,
     });
-    console.log(req.body)
+    const mailData = {
+        from: 'elanafrica753@gmail.com',
+        to: 'team@elanafrica.com',
+        subject: `Message From ${req.body.name}`,
+        text: req.body.message + " | Sent from: " + req.body.email,
+        html: `<div>${req.body.message}</div><p>Sent from:
+        ${req.body.email}</p>`
+      }
+       transporter.sendMail(mailData, function (err, info) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info)
+      })
+
+      res.status(200)
 }
